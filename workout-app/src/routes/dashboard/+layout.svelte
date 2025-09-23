6fa7811beb742d64f4ed1b37832ff8abe1a7786d
Mr-LMN <lloydmartin94@gmail.com>
2025-09-23 23:20:03 +0100

    Replace auth guard layouts with Svelte-based guard

---
+++ workout-app/src/routes/dashboard/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { user, loading } from '$lib/store';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
+
+	$: if (!$loading && !$user) {
+		goto(resolve('/'));
+	}
+</script>
+
+{#if $user}
+	<slot />
+{:else}
+	<p>Verifying access...</p>
+{/if}
